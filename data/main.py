e3e396d3db26f27f73982ba32a5d3adf87decb27
2005jananisreer-prog <2005jananisree.r@gmail.com>
2026-03-25 22:50:20 +0530

    Update main.py

--- data/main.py
+++ data/main.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+print("Lazarus Medical Recovery System Running...")
+
+data = {"Patient": ["A", "B"], "BPM": [80, None]}
+df = pd.DataFrame(data)
+
+df["BPM"] = df["BPM"].fillna(method="ffill")
+
+print(df)
